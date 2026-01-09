ee49d695e714830318c7807f94fbe8dea917e1b7
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-01-09 14:19:34 -0500

    users practice update

--- users.py
+++ users.py
@@ -1,3 +1,5 @@
+
+#starting my website with usernames
 userNames = ['admin', 'gameGuy123', 'proGamer', 'noobMaster69', 'eliteSniper', 'speedRunner'];
 for userName in userNames:
     if userName == 'admin':
@@ -7,4 +9,25 @@ for userName in userNames:
 if not userNames:
     print("We need to find some users!");
 
-#com
+#checking if usernames are unique
+currentUsers = ['admin', 'gameGuy123', 'proGamer', 'noobMaster69', 'eliteSniper', 'speedRunner'];
+newUsers = ['ProGamer', 'newUser1', 'speedrunner', 'gamerGirl', 'montyPy'];
+Users = [item.lower() for item in currentUsers]; #this is an inline way of making the items lowercase by copying the list above.
+for user in newUsers:
+    if user.lower() in Users:
+        print(f"The Username {user}, is taken")
+    else:
+        print(f"Welcome, {user}")
+
+#Ordinal Numbers
+numberList = [1,2,3,4,5,6,7,8,9]
+
+for number in numberList:
+    if number == 1:
+        print(f'{number}st')
+    elif number == 2:
+        print(f'{number}nd')
+    elif number == 3:
+        print(f'{number}rd')
+    else:
+        print(f'{number}th')
