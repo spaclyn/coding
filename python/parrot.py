8fd41304ed59c923de21edeb29b599db4394fff4
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-01-21 10:45:40 -0500

    more dail exercise

---
+++ python/parrot.py
@@ -0,0 +1,11 @@
+message = input("Tell me something, and I will repeat it back to you: ")
+print(message)
+
+#greeter.py
+name = input("Please enter your name: ")
+print(f"Hello, {name}") #Dont need a linebreak here, python automatically does that in terminal
+
+prompt = "If you share your name, we can personalize the message you see."
+prompt += "\n What is your first name? "
+name = input(prompt)
+print(f"Hello again, {name}")
