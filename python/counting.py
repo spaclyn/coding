418deb9ef3acfafd8dea47dffffa60be292bb27c
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-01-21 15:23:39 -0500

    some more work for today

---
+++ python/counting.py
@@ -0,0 +1,4 @@
+currentNum = 1
+while currentNum <= 5:
+    print(currentNum)
+    currentNum = currentNum + 1
--- python/parrot.py
+++ python/parrot.py
@@ -1,5 +1,12 @@
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+prompt = "\nTell me something, and I will repeat it back to you: "
+prompt += "\nEnter 'quit' to end the program. "
+message = " "
+while message != 'quit':
+    message = input(prompt)
+
+    if message != 'quit':
+        print(message)
+
 
 #greeter.py
 name = input("Please enter your name: ")
