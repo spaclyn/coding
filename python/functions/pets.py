948d51854518b10ea62e6e12354c1b4367305999
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-01-29 12:13:58 -0500

    more functions

--- python/functions/pets.py
+++ python/functions/pets.py
@@ -6,3 +6,14 @@ def describePets(animalType , petName):     #a arguement with a default value ca
 describePets('hamster', 'harry')
 describePets(animalType = 'dog', petName = 'Fido')
 
+def makeshirt(size, text):
+    """Display information about a shirt"""
+    print(f"\nYou have ordered a shirt of size {size} with the text '{text}' printed on it.")
+
+makeshirt('L', 'Sweet Shirt');
+makeshirt(size = 'L', text = "Shirt");
+
+def makeLargeShirt(text, size = 'L'):
+    """Display information about a large shirt"""
+    print(f"\nYou have ordered a shirt of size {size} with the text '{text}' printed on it.")
+
