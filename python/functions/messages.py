02c14b98c057131c8b492732db9408848ff3138f
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-02-11 13:26:47 -0500

    more function practice

---
+++ python/functions/messages.py
@@ -0,0 +1,30 @@
+messags = ["Hi Guys",
+            "Who's ready to lean Python?",
+            "Do you like programming? I do!"];
+
+def showMessages(messags):
+    """Prints a serious of short messages."""
+    for messag in messags:
+        print(messag);
+showMessages(messags);
+
+greetings = ['Hey There',
+             'Welcome to the World of Python',
+             'Hello World']
+showMessages(greetings);
+
+def sendMessages(messages, sentMessages):
+    """SImulate sending each message, until none left.
+    Move each messae to sentMessages after sending/printing."""
+    while messages:
+        unsentMessage = messages.pop();
+        print(f"Sending Message: {unsentMessage}");
+        sentMessages.append(unsentMessage);
+
+messages = ["Hi Guys",
+            "Who's ready to lean Python?",
+            "Do you like programming? I do!"];
+sentMessages = [];
+sendMessages(messages[:], sentMessages);
+print(messages);
+print(sentMessages);
--- python/functions/printingModels.py
+++ python/functions/printingModels.py
@@ -13,3 +13,21 @@ while unprintedDesigns:
 print("\nThe following models have been printed: ");
 for completedModel in completedModels:
     print(completedModel);
+
+
+def printModels(unprintedDesigns, completedModels):
+    """Simulate printing each design, until none are left.
+    Move each design to completedModels after printing."""
+    while unprintedDesigns:
+        currentDesign = unprintedDesigns.pop();
+        print(f"Printing model: {currentDesign}");
+        completedModels.append(currentDesign);
+
+def showCompletedModels(completedModels):
+    """Show all the models that were printed."""
+    print("\nThe following models have been printed: ");
+    for completedModel in completedModels:
+        print(completedModel);
+
+printModels(unprintedDesigns[:], completedModels);
+showCompletedModels(completedModels)
