083436c49add6d41fe0d14284bc76d1b7c5bbbfe
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-02-11 12:23:29 -0500

    list modifcation

---
+++ python/functions/printingModels.py
@@ -0,0 +1,15 @@
+#start with some  designs that need to be printed.
+unprintedDesigns = ['phone case', 'robot pendant', 'dodecahedron'];
+completedModels = [];
+
+#simulate printing each design, until none are left.
+# move each design to completedModels after printing.
+while unprintedDesigns:
+    currentDesign = unprintedDesigns.pop();
+    print(f"Printing model: {currentDesign}");
+    completedModels.append(currentDesign);
+
+# display all completed models.
+print("\nThe following models have been printed: ");
+for completedModel in completedModels:
+    print(completedModel);
