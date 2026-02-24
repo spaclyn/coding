4ac08a4b18b9a7d0f9c19ebfdc881da15357103d
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-02-24 14:27:37 -0500

    added attributes and new functions

--- python/classes/restaurant.py
+++ python/classes/restaurant.py
@@ -1,10 +1,11 @@
 class Restaurant:
     """A simple attempt to model a resturant."""
     
-    def __init__(self, resturantName, cuisineType):
+    def __init__(self, resturantName, cuisineType, ):
         """Initialize resturant name and cuisine type attributes."""
         self.name = resturantName;
         self.cuisineType = cuisineType;
+        self.numberServed = 0;
     
     def describeRestaurant(self):
         """Simulate Describe the resturant."""
@@ -13,6 +14,18 @@ class Restaurant:
     def openRestaurant(self):
         """Simulate opening the restaurant."""
         print(f"{self.name} is now open!");
+
+    def readNumberServed(self):
+        """Print a statement showing the number of customers served."""
+        print(f"{self.name} has served {self.numberServed} customers.");
+
+    def setNumberServed(self, number):
+        """Set the number of customers that have been served."""
+        self.numberServed = number;
+
+    def incrementNumberServed(self, additionalServed):
+        """Add the given amount to the number of customers served."""
+        self.numberServed += additionalServed;
 
 myRestaurant = Restaurant('Pizza Hut', 'Italian');
 myRestaurant.describeRestaurant();
@@ -26,3 +39,17 @@ theirRestaurant = Restaurant('Sushi Boss', 'Japanese');
 theirRestaurant.describeRestaurant();
 theirRestaurant.openRestaurant();
 
+myNewRestaurant = Restaurant('Homeward Bound', 'Southern Fusion');
+myNewRestaurant.describeRestaurant();
+myNewRestaurant.openRestaurant();
+
+myNewRestaurant.numberServed = 10;
+myNewRestaurant.readNumberServed();
+myNewRestaurant.numberServed = 25;
+myNewRestaurant.readNumberServed();
+
+myNewRestaurant.setNumberServed(100);
+myNewRestaurant.readNumberServed();
+
+myNewRestaurant.incrementNumberServed(100);
+myNewRestaurant.readNumberServed();
