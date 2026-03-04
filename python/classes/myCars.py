0ad689b47475fb90fa537febae5812e60b53adef
Atiyah Ellerbee <80544277+spaclyn@users.noreply.github.com>
2026-03-04 11:51:14 -0500

    more module importing classes

---
+++ python/classes/myCars.py
@@ -0,0 +1,8 @@
+import car
+
+myMustang = car.Car('ford', 'mustang', 2021);
+print(myMustang.getDescriptiveName());
+
+
+myTesla = car.ElectricCar('Tesla', 'Model Y', 2024);
+print(myTesla.getDescriptiveName());
